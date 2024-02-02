0df2cc210b115e3c3d77e23d913bed042d296f3f
MOsamir19 <106157548+MOsamir19@users.noreply.github.com>
2024-02-02 03:37:14 +0200

    main.c

--- Master/main.c
+++ Master/main.c
@@ -1,39 +1,23 @@
-/*
- * main.c
- *
- *  Created on: Sep 25, 2022
- *      Author: General
- */
-
-
+/*****************************************************************************
+ *==================******************************************================*
+ *==================**   Author: Mohamed Samir Hashish      **================*
+ *==================**               NTI                    **================*
+ *==================******************************************================*
+ /*****************************************************************************/
 
 
 #include "STD_TYPES.h"
 #include "BIT_MATH.h"
 #include "util/delay.h"
-
 #include "DIO_Interface.h"
-#include "EXTINT_Interface.h"
 #include "UART_Interface.h"
 #include "SPI_Interface.h"
-#include "GIE_Interface.h"
-#include "ADC_Interface.h"
-#include "TMR0_Interface.h"
-#include "KPD_Interface.h"
 #include "LCD_Interface.h"
 
-#include "TWI_int.h"
-#include "EXT_EEPROM_int.h"
 
 #ifndef F_CPU
 #define F_CPU 8000000UL
 #endif
-//#include "avr/io.h"
-//#undef  F_CPU
-//#define F_CPU (16000000UL)
-
-
-
 
 int main()
 {
